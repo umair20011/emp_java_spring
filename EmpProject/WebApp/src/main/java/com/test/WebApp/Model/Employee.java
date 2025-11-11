5ad4f3309069812762ab15390b1554baff50cf79
umair20011 <danishmaniyar2109@gmail.com>
2025-11-11 15:10:48 +0530

    created fields, constructors, and getters and setters for Employee class

--- EmpProject/WebApp/src/main/java/com/test/WebApp/Model/Employee.java
+++ EmpProject/WebApp/src/main/java/com/test/WebApp/Model/Employee.java
@@ -1,5 +1,54 @@
 package com.test.WebApp.Model;
 
+
 public class Employee {
 
+	private int empId;
+	private String empName;
+	private int empSalary;
+	private String empDepartment;
+	
+	Employee(int empId, String empName, int empSalary, String empDepartment){
+		
+		this.empId=empId;
+		this.empName=empName;
+		this.empSalary=empSalary;
+		this.empDepartment=empDepartment;
+		
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	public String getEmpDepartment() {
+		return empDepartment;
+	}
+
+	public void setEmpDepartment(String empDepartment) {
+		this.empDepartment = empDepartment;
+	}
+	
+	
+	
 }

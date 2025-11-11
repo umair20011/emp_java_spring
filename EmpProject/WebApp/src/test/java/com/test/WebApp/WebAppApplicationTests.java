c7c2683555fbfd2d21442423cfe0e6b241dc1590
umair20011 <danishmaniyar2109@gmail.com>
2025-11-11 14:26:25 +0530

    created a spring boot project

---
+++ EmpProject/WebApp/src/test/java/com/test/WebApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
